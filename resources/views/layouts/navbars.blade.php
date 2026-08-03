1885215139d1f7cb23cfe9df230d1ef5e36190bc
AVO Aniel <andriafanomezantsoa96@gmail.com>
2026-08-03 06:36:21 +0200

    Fix admin layout vite css

--- resources/views/layouts/navbars.blade.php
+++ resources/views/layouts/navbars.blade.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body
