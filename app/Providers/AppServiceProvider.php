02b30a061b5edf857e994bd602a0bf424f9dd184
AVO Aniel <andriafanomezantsoa96@gmail.com>
2026-08-03 06:12:58 +0200

    Fix admin navbar notifications variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Notification;
+use Illuminate\Support\Facades\Auth;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +17,18 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+        View::composer('admin.partials.navbar', function ($view) {
+    
+            $notifications = collect();
+    
+            if (Auth::check()) {
+                $notifications = Notification::where('user_id', Auth::id())
+                    ->latest()
+                    ->take(5)
+                    ->get();
+            }
+    
+            $view->with('notifications', $notifications);
+        });
     }
 }
