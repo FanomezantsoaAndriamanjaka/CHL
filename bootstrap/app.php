1dd8cd43cef78ef31c0b8ffb101ac22f8da42a0e
AVO Aniel <andriafanomezantsoa96@gmail.com>
2026-08-07 21:57:11 +0200

    Fix HTTPS detection for Render proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,10 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
 
+        $middleware->trustProxies(at: '*');
+    
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
-
+    
     })
 
     ->withExceptions(function (Exceptions $exceptions) {
